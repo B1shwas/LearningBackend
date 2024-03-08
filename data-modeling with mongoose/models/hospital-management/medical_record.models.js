4df678f2c27232feb75cc3dfe2f1df76707bf262
Bishwash Deoju <b1shwas@MacBook-Air.local>
2024-03-08 13:25:38 +0545

    removed other folders

--- data-modeling with mongoose/models/hospital-management/medical_record.models.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-const medicalRecordSchema = new mongoose.Schema({}, { timestamps: true });
-export const MedicalRecord = mongoose.model(
-  "MedicalRecord",
-  medicalRecordSchema
-);
